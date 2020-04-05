17310ad50eea45150c47286a8c0f7f6abfb89b57
Lester Jan Artienda <artiele@oocl.com>
2020-04-05 22:49:54 +0800

    Draft initial template

---
+++ src/components/CasesTracker.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="6" class="world">
+        <h3>World Statistics</h3>
+      </v-col>
+      <v-col cols="6" class="country">
+        <h3>Country Statistics</h3>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "CasesTracker",
+
+  data: () => {}
+};
+</script>
